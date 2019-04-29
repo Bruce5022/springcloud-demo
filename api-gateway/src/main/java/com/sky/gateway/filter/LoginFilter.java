c1dc413c24d154b43ca235d79670b7c7c2b22d14
szw <shizhanwei123@aliyun.com>
2019-04-30 07:56:48 +0800

    增加网关过滤器

---
+++ api-gateway/src/main/java/com/sky/gateway/filter/LoginFilter.java
@@ -0,0 +1,42 @@
+package com.sky.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LoginFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 4;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String requestURI = request.getRequestURI();
+        System.out.println("URI地址:" + requestURI);
+        StringBuffer requestURL = request.getRequestURL();
+        System.out.println("URL地址:" + requestURL);
+        if("/gateway/order/api/order/save".equalsIgnoreCase(requestURI)){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("执行方法体内容...");
+        return null;
+    }
+}
