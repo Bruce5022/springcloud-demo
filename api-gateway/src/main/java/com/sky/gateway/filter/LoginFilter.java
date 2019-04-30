6cc25eec2c0019c4db2d8da37a3f97e6a7923ca5
szw <shizhanwei123@aliyun.com>
2019-04-30 08:10:37 +0800

    增加网关过滤器

--- api-gateway/src/main/java/com/sky/gateway/filter/LoginFilter.java
+++ api-gateway/src/main/java/com/sky/gateway/filter/LoginFilter.java
@@ -4,7 +4,9 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,7 +30,7 @@ public class LoginFilter extends ZuulFilter {
         System.out.println("URI地址:" + requestURI);
         StringBuffer requestURL = request.getRequestURL();
         System.out.println("URL地址:" + requestURL);
-        if("/gateway/order/api/order/save".equalsIgnoreCase(requestURI)){
+        if ("/gateway/order/api/order/save".equalsIgnoreCase(requestURI)) {
             return true;
         }
         return false;
@@ -37,6 +39,14 @@ public class LoginFilter extends ZuulFilter {
     @Override
     public Object run() throws ZuulException {
         System.out.println("执行方法体内容...");
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String accessToken = request.getHeader("access-token");
+        System.out.println("accessToken:" + accessToken);
+        if (StringUtils.isEmpty(accessToken)) {
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+        }
         return null;
     }
 }
